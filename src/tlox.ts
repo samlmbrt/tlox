af509773bdaf8a7783be9281f40d5957593da121
Samuel Lambert <samuel.lambert@hotmail.com>
2021-05-23 17:48:06 -0400

    Add a Token class, which will store the lexeme/type information

--- src/tlox.ts
+++ src/tlox.ts
@@ -1,14 +1,28 @@
 import { readFileSync } from 'fs';
 import { createInterface } from 'readline';
+import { TokenType, Token } from './token';
 
 enum ErrorCode {
   BAD_USAGE = 1,
   INVALID_FILE,
+  INVALID_CODE,
 }
+
+const error = (line: number, message: string) => {
+  report(line, '', message);
+};
+
+const report = (line: number, where: string, message: string) => {
+  console.log(`[line: ${line}] error ${where}: ${message}`);
+  hadError = true;
+};
 
 const runFile = (filePath: string) => {
   try {
     run(readFileSync(filePath, 'utf8'));
+    if (hadError) {
+      process.exit(ErrorCode.INVALID_CODE);
+    }
   } catch (err) {
     console.error(`Could not open file: ${filePath}`);
     process.exit(ErrorCode.INVALID_FILE);
@@ -23,6 +37,7 @@ const runPrompt = () => {
 
   rl.on('line', (line: string) => {
     run(line);
+    hadError = false;
     rl.prompt();
   });
 
@@ -36,6 +51,7 @@ const run = (source: string) => {
 
 // todo: use a better commnand-line parser when necessary
 const commandLineArguments = process.argv.slice(2);
+let hadError = false;
 
 if (commandLineArguments.length > 1) {
   console.error('Usage: tlox [file]');
---
+++ src/token.ts
@@ -0,0 +1,64 @@
+enum TokenType {
+  // Single-character tokens
+  LEFT_PAREN,
+  RIGHT_PAREN,
+  LEFT_BRACE,
+  RIGHT_BRACE,
+  COMMA,
+  DOT,
+  MINUS,
+  PLUS,
+  SEMICOLON,
+  SLASH,
+  STAR,
+
+  // On or two character tokens
+  BANG,
+  BANG_EQUAL,
+  EQUAL,
+  EQUAL_EQUAL,
+  GREATER,
+  GREATER_EQUAL,
+  LESS,
+  LESS_EQUAL,
+
+  // Literals
+  IDENTIFIER,
+  STRING,
+  NUMBER,
+
+  // Keywords
+  AND,
+  CLASS,
+  ELSE,
+  FALSE,
+  FUN,
+  FOR,
+  IF,
+  NIL,
+  OR,
+  PRINT,
+  RETURN,
+  SUPER,
+  THIS,
+  TRUE,
+  VAR,
+  WHILE,
+
+  EOF,
+}
+
+class Token {
+  constructor(
+    private tokenType: TokenType,
+    private lexeme: string,
+    private literal: string,
+    private line: number
+  ) {}
+
+  toString(): string {
+    return `${this.tokenType} ${this.lexeme} ${this.literal} ${this.line}`;
+  }
+}
+
+export { TokenType, Token };
