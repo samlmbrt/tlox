dd2bf6e868eb419f7212d2606fff62caa35ff303
Samuel Lambert <samuel.lambert@hotmail.com>
2021-05-30 21:38:26 -0400

    Add Lox grammar as a comment for reference

--- src/parser.ts
+++ src/parser.ts
@@ -1,3 +1,13 @@
+// Lox grammar
+//
+// expression    -> equality
+// equality      -> comparison (("!= | ==") comparison)*;
+// comparison    -> term ((">" | ">= | "<" | "<="") term)*;
+// term          -> factor (("-" | "+") factor)*;
+// factor        -> unary (("/" | "*") unary)*;
+// unary         -> ("!" | "-") unary | primary;
+// primary       -> NUMBER | STRING | "true" | "false" | "nil" | "(" expression ")";
+
 import { BinaryExpression, Expression } from './expression';
 import { Token, TokenType } from './token';
 
